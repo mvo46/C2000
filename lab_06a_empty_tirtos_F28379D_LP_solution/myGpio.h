5421357a05ee2b5a209c32ab03be7b3184b47658
Minh Vo <minh.vo0817@gmail.com>
2023-01-16 14:07:12 -0800

    Empty TI_RTOS Project

---
+++ lab_06a_empty_tirtos_F28379D_LP_solution/myGpio.h
@@ -0,0 +1,16 @@
+/*
+ * myGpio.h
+ *
+ *  Created on: Jan 16, 2023
+ *      Author: Katie Nguyen
+ */
+
+#ifndef MYGPIO_H_
+#define MYGPIO_H_
+
+#include "board.h"
+
+#define GPIO_PIN_LED_BLUE GPIO_10_GPIO10
+
+
+#endif /* MYGPIO_H_ */
